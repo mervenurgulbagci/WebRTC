d4ae13c1bbd0babeb42e4dbdb68e0e82dadb2d5c
Merve Nurgül Bağcı <mervenurgulbagci@gmail.com>
2020-12-14 01:31:49 +0300

    Initial commit

---
+++ src/main/java/com/mnb/WebRTC/WebSocketConfiguration.java
@@ -0,0 +1,23 @@
+package com.mnb.WebRTC;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import java.io.IOException;
+import java.util.logging.SocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer{
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        try {
+            registry.addHandler(new SocketHandler(),"/socket").setAllowedOrigins("*");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
